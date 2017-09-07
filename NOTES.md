cd397d894179b0bb725d82fbea5f25cdc5fbcff1
Stacey Vetzal <stacey@vetzal.com>
2017-09-07 19:25:24 -0400

    let's add a validator for input elements on the path to the next test

--- NOTES.md
+++ NOTES.md
@@ -9,4 +9,11 @@ then extending that out to a service, and then a quick front-end.
   linter with the way mocha runs the tests - pre v3 mocha I used to be able to
   import describe and it in order to not leave hanging undefined method refs
   in the test code :-(
-
+- heh, .deep.equal does value equality :) .equal does object quality
+- working through RPN logic, maybe let's make the method that calculates the
+  result of a stack of numbers/operations a pure function
+- want a validator for the items on the stack; what types should we allow there?
+  strings & numerics? just strings?
+  - let's allow numerics unless this seems to get hairy
+  - yeah, I like regex, looks a bit "clever", but I'll leave it for now, use a
+    a var name to express the intent
--- src/RPNLogic.spec.js
+++ src/RPNLogic.spec.js
@@ -3,7 +3,13 @@ const expect = require('chai').expect;
 class RPNLogic {
   reduce(input) {
     if (!Array.isArray(input)) throw('input must be an array');
+
     return input;
+  }
+
+  isValid(input) {
+    let validOperator = /[+-/*]/;
+    return typeof(input) === 'number' || input.match(validOperator) !== null;
   }
 }
 
@@ -22,4 +28,13 @@ describe('RPNLogic', () => {
   it('should return an empty array if input is empty', () => {
     expect(rpn.reduce([])).to.deep.equal([]);
   });
+
+  it('should have a method to determine if an element in the array is valid', () => {
+    expect(rpn.isValid('a')).to.equal(false);
+  });
+
+  xit('should not accept invalid characters in the array (only 0-9,+,/,*,-)', () => {
+    expect(() => rpn.reduce(['hello'])).to.throw();
+  });
+
 });
