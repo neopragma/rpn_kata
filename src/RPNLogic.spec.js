d0a00c85e0708c61ae394d947c8ee1fbf642aa6a
Stacey Vetzal <stacey@vetzal.com>
2017-09-07 18:59:54 -0400

    an empty queue reduces to an empty queue

--- src/RPNLogic.spec.js
+++ src/RPNLogic.spec.js
@@ -3,12 +3,23 @@ const expect = require('chai').expect;
 class RPNLogic {
   reduce(input) {
     if (!Array.isArray(input)) throw('input must be an array');
+    return input;
   }
 }
 
 describe('RPNLogic', () => {
+
+  let rpn;
+
+  beforeEach(() => {
+    rpn = new RPNLogic();
+  });
+
   it('should throw if the input is not an array', () => {
-    const rpn = new RPNLogic();
     expect(() => rpn.reduce()).to.throw();
+  });
+
+  it('should return an empty array if input is empty', () => {
+    expect(rpn.reduce([])).to.deep.equal([]);
   });
 });
