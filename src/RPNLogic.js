efb7b3886e8ba38c6f603104b4303c7bc081ba39
Stacey Vetzal <stacey@vetzal.com>
2017-09-09 20:15:17 -0400

    a bit of refactoring to clean up

--- src/RPNLogic.js
+++ src/RPNLogic.js
@@ -6,24 +6,32 @@ class RPNLogic {
   }
 
   reduce(input) {
-    if (!Array.isArray(input))
-      throw('input must be an array');
-    if (!this.isArrayValid(input))
-      throw('input must contain all valid elements');
+    if (!Array.isArray(input)) throw('input must be an array');
+    if (!this.isArrayValid(input)) throw('input must contain all valid elements');
 
     let stack = [];
     input.forEach(el => {
       if (typeof(el) === 'number') {
         stack.push(el);
       } else {
-        let operandA = stack.pop();
-        let operandB = stack.pop();
-        if (typeof(operandA) === 'undefined' || typeof(operandB) === 'undefined')
-          throw(`not enough operands for operator ${el}`);
+        let {operandA, operandB} = this.retrieveOperandPair(stack);
       }
     });
 
     return stack;
+  }
+
+  retrieveOperandPair(stack) {
+    return {
+      operandA: this.retrieveOperand(stack),
+      operandB: this.retrieveOperand(stack)
+    };
+  }
+
+  retrieveOperand(stack) {
+    let operand = stack.pop();
+    if (typeof(operand) === 'undefined') throw('not enough operands for operator');
+    return operand;
   }
 
   isArrayValid(input) {
