fdd179ec3ef97aa8accd229b8d8b70222b45dc88
Stacey Vetzal <stacey@vetzal.com>
2017-09-13 13:00:53 -0400

    catching up systemjs branch

--- www/main.js
+++ www/main.js
@@ -1,23 +1,4 @@
-class RPNGateway {
-  get headers() {
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    headers.append('Accept', 'application/json');
-    return headers;
-  }
-
-  reduce(values) {
-    return fetch('/api/v1/reduce', {
-      method: 'POST',
-      body: JSON.stringify(values),
-      headers: this.headers
-    })
-      .then(response => {
-        if (response.status === 400) throw('Invalid input.');
-        return response.json();
-      });
-  }
-}
+import RPNGateway from './RPNGateway';
 
 const initialState = {
   error: false,
