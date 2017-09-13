2abd38974431fc8f6c75145eb07a44134ab01324
Stacey Vetzal <stacey@vetzal.com>
2017-09-13 12:59:14 -0400

    oops, leaked some systemjs stuff into this; backing it off so things are functional again :\ should've branched

--- www/main.js
+++ www/main.js
@@ -1,5 +1,23 @@
-import RPNGateway from 'RPNGateway';
-// const RPNGateway = require('RPNGateway');
+class RPNGateway {
+  get headers() {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    return headers;
+  }
+
+  reduce(values) {
+    return fetch('/api/v1/reduce', {
+      method: 'POST',
+      body: JSON.stringify(values),
+      headers: this.headers
+    })
+      .then(response => {
+        if (response.status === 400) throw('Invalid input.');
+        return response.json();
+      });
+  }
+}
 
 const initialState = {
   error: false,
